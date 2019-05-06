93e0e1cda13adbf8e65c3b7d6fa6fb922c0ddfce
Sergey <m.shamkov.sergei@yandex.ru>
2019-05-06 14:11:27 +0300

    Finished login

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const logger = require("morgan");
 const expressSession = require("express-session");
 const expressValidator = require("express-validator");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const registerRouter = require("./routes/register");
 const usersRouter = require("./routes/users");
 const indexRouter = require("./routes/index");
+const loginRouter = require("./routes/login");
 
 const app = express();
 
@@ -34,25 +36,31 @@ app.use(
   })
 );
 
+require("./config/passport");
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get("/", indexRouter);
 app.get("/users", usersRouter.form);
 app.get("/register", registerRouter.form);
 app.post("/register", registerRouter.submit);
+app.get("/login", loginRouter.form);
+app.post("/login", loginRouter.submit);
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 module.exports = app;
